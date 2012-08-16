8a30e8d3e922a1d34ab99c99363d2a80221d9a44
Beomki Lee <c0untd0wn@wafflestudio.com>
2012-08-16 22:04:48 +0900

    * remove appkit from setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "includes": ["AppKit"], "excludes": ["tkinter"], "include_files": [("cat.png", "cat.png"), ("cat.ico", "cat.ico"), ("setting.json", "setting.json")], "icon": "cat.ico"}
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"], "include_files": [("cat.png", "cat.png"), ("cat.ico", "cat.ico"), ("setting.json", "setting.json")], "icon": "cat.ico"}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
